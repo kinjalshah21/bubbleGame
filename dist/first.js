a4da248a7d4f430eebb13612a86ea31f58a18e28
Kinjal_Shah <kinjalshah83415@gmail.com>
2023-10-05 23:27:57 +0530

    starting page added and script modified.

---
+++ dist/first.js
@@ -0,0 +1,5 @@
+// const playButton = document.getElementById("play-button");
+
+// playButton.addEventListener("click", function() {
+//     window.location.href = "./dist/game.html";
+// });
--- dist/script.js
+++ dist/script.js
@@ -6,6 +6,22 @@ function increaseScore() {
   score += 10;
   document.querySelector("#score-val").textContent = score;
 }
+function decreaseScore() {
+  score -= 1;
+  document.querySelector("#score-val").textContent = score;
+
+  if (score < -5) {
+    document.querySelector(
+      "#panel-bottom"
+    ).innerHTML = `<div class="text-[#231942] text-3xl w-full h-full font-bold flex justify-center items-center">
+        <h2>Better Luck Next Time!</h2>
+    </div>`;
+  }
+}
+function increaseTimer() {
+  timer += 1;
+  document.querySelector("#time-val").textContent = timer;
+}
 
 function getNewHit() {
   num = Math.floor(Math.random() * 10);
@@ -33,24 +49,28 @@ function runTimer() {
     if (timer > 0) {
       timer--;
       document.querySelector("#time-val").textContent = timer;
-    } else{
-        clearInterval(timerValue);
-        document.querySelector("#panel-bottom").innerHTML = `<div class="text-[#231942] text-3xl w-full h-full font-bold flex justify-center items-center">
+    } else {
+      clearInterval(timerValue);
+      document.querySelector(
+        "#panel-bottom"
+      ).innerHTML = `<div class="text-[#231942] text-3xl w-full h-full font-bold flex justify-center items-center">
         <h2>Your Score: ${score}</h2>
     </div>`;
     }
-    ;
   }, 1000);
 }
 
 document
   .querySelector("#panel-bottom")
   .addEventListener("click", function (details) {
-    let clickedNum = Number((details.target.textContent));
-    if(clickedNum == num){
-        increaseScore();
-        getNewHit();
-        makeBubble();
+    let clickedNum = Number(details.target.textContent);
+    if (clickedNum == num) {
+      increaseTimer();
+      increaseScore();
+      getNewHit();
+      makeBubble();
+    } else {
+      decreaseScore();
     }
   });
 
